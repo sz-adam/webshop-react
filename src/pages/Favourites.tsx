7f6c526c4f82544b99e3eb69b736a5d5e4807220
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-08-01 17:32:34 +0200

    creating a favorites page

---
+++ src/pages/Favourites.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface Props {
+  
+}
+
+const Favorites: React.FC<Props> = (props) => {
+  return (
+    <div>
+     Favorites
+    </div>
+  );
+};
+
+export default Favorites;
