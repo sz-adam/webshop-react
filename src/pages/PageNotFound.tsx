554589475c6de72944e1fa63c6381fbf4c1b82db
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-08-01 16:53:29 +0200

    create pageNotFound

--- src/pages/PageNotFound.tsx
+++ src/pages/PageNotFound.tsx
@@ -1,18 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { SiBasicattentiontoken } from "react-icons/si";
 
 const PageNotFound: React.FC = () => {
-  return <div className="text-center">
-  <h1 className="mb-4 text-9xl font-semibold text-red-500">404</h1>
-  <p className="mb-4 text-lg text-gray-600">Oops! Looks like you're lost.</p>
-  <div className="animate-bounce">
-    <svg className="mx-auto h-16 w-16 text-red-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-      <path strokeLinecap="round"  strokeWidth="2" d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"></path>
-    </svg>
-  </div>
-  <p className="mt-4 text-gray-600">Let's get you back <Link to="/" className="text-blue-500">home</Link>.</p>
-</div>
-
+  return (
+    <div className="text-center">
+      <h1 className="mb-4 text-9xl font-semibold text-red-500">404</h1>
+      <p className="mb-4 text-lg text-gray-600">
+        Oops! Looks like you're lost.
+      </p>
+      <div className="animate-bounce">
+        <SiBasicattentiontoken className="mx-auto h-16 w-16 text-red-500" />
+      </div>
+      <p className="mt-4 text-gray-600">
+        Let's get you back{" "}
+        <Link to="/" className="text-blue-500">
+          home
+        </Link>
+        .
+      </p>
+    </div>
+  );
 };
 
 export default PageNotFound;
