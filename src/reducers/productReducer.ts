50ea512432fda123ee06dd2b5306b1331305c429
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-08-01 17:13:31 +0200

    get all products

--- src/reducers/productReducer.ts
+++ src/reducers/productReducer.ts
@@ -1,24 +1,26 @@
-// reducer.ts
-
 import { Product } from "../types/product";
 
+//reducer állapotok
 interface State {
   products: Product[];
   loading: boolean;
   error: string | null;
 }
 
+//reducer álltal kezelt akciók
 type Action =
   | { type: "FETCH_PRODUCTS_REQUEST" }
   | { type: "FETCH_PRODUCTS_SUCCESS"; payload: Product[] }
   | { type: "FETCH_PRODUCTS_FAILURE"; payload: string };
 
+
+  //kezdő értékek
 export const initialState: State = {
   products: [],
   loading: false,
   error: null,
 };
-
+// A reducer függvény kezeli az állapot változását az akciók hatására.
 export function reducer(state: State, action: Action): State {
   switch (action.type) {
     case "FETCH_PRODUCTS_REQUEST":
