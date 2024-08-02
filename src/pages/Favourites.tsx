0d783a1e04b6595e8bee3966c66b0a65b276f3eb
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-08-02 17:29:18 +0200

    add flex wrap

--- src/pages/Favourites.tsx
+++ src/pages/Favourites.tsx
@@ -6,7 +6,7 @@ const Favorites: React.FC = () => {
   const { favorites } = useFavorites();
 
   return (
-    <div>
+    <div className="flex flex-wrap justify-center">
       {favorites.length === 0 ? (
        <div className="flex justify-center text-center">
          <p className="font-bold text-4xl text-red-600 mt-10">No favorite products found.</p>
