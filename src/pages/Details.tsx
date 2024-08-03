101bfa3e7fef3ca8eff7e9f404ea4eccf6a044d9
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-08-03 17:29:27 +0200

    retrieval and display of data

--- src/pages/Details.tsx
+++ src/pages/Details.tsx
@@ -1,7 +1,119 @@
-import React from "react";
+import React, { useEffect, useReducer } from "react";
+import { useParams } from "react-router-dom";
+import { Product } from "../types/product";
+import Loading from "../components/Loading";
+import { initialState, reducer } from "../reducers/detailsReducer";
+import { fetchProductById } from "../services/productService";
+import { FaStar } from "react-icons/fa";
+import { CiHeart } from "react-icons/ci";
+import { FaCartArrowDown } from "react-icons/fa";
 
 const Details: React.FC = () => {
-  return <h2>Details</h2>;
+  const { id } = useParams<{ id: string }>();
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  // Az adatok lekérésére szolgáló useEffect
+  useEffect(() => {
+    const getProduct = async () => {
+      dispatch({ type: "FETCH_DETAILS_REQUEST" });
+      try {
+        const product: Product = await fetchProductById(Number(id));
+        dispatch({ type: "FETCH_DETAILS_SUCCESS", payload: product });
+      } catch (error) {
+        dispatch({
+          type: "FETCH_DETAILS_FAILURE",
+          payload: (error as Error).message,
+        });
+      }
+    };
+
+    getProduct();
+  }, [id]);
+
+  const { details, loading, error } = state;
+
+  return (
+    <div>
+      {error && <p>Error: {error}</p>}
+      {loading && (
+        <div>
+          <Loading />
+        </div>
+      )}
+      {details && (
+        <section className="py-8 md:py-16 ">
+          <div className="max-w-screen-xl px-4 mx-auto 2xl:px-0">
+            <div className="lg:grid lg:grid-cols-2 lg:gap-8 xl:gap-16">
+              <div className="max-w-md lg:max-w-lg mx-auto">
+                <img
+                  className="w-full"
+                  src={details.image}
+                  alt={details.title}
+                />
+              </div>
+
+              <div className="mt-6 sm:mt-8 lg:mt-0">
+                <h1 className="text-xl font-semibold text-gray-900 sm:text-2xl ">
+                  {details.title}
+                </h1>
+                <div className="mt-4 sm:items-center sm:gap-4 sm:flex">
+                  <p className="text-2xl font-extrabold text-gray-900 sm:text-3xl">
+                    {details.price} $
+                  </p>
+
+                  <div className="flex items-center gap-2 mt-2 sm:mt-0">
+                    {/**rating */}
+                    <div className="flex items-center gap-1">
+                      <FaStar />
+                      <FaStar />
+                      <FaStar />
+                      <FaStar />
+                      <FaStar />
+                    </div>
+                    <p className="text-sm font-medium  text-gray-500 ">
+                      {details.rating.rate}
+                    </p>
+                    <a
+                      href="#"
+                      className="text-sm font-medium  text-gray-900 underline hover:no-underline "
+                    >
+                      {details.rating.count} Reviews
+                    </a>
+                  </div>
+                </div>
+
+                <div className="mt-6 sm:gap-4 sm:items-center sm:flex sm:mt-8">
+                  <a
+                    href="#"
+                    title=""
+                    className="flex items-center justify-center py-2.5 px-5 text-sm font-medium text-gray-900 focus:outline-none rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-100"
+                    role="button"
+                  >
+                    <CiHeart className="w-5 h-5 -ms-2 me-2" />
+                    Add to favorites
+                  </a>
+
+                  <a
+                    href="#"
+                    title=""
+                    className="mt-4 sm:mt-0 bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 focus:outline-none flex items-center justify-center"
+                    role="button"
+                  >
+                    <FaCartArrowDown className="w-5 h-5 -ms-2 me-2" />
+                    Add to cart
+                  </a>
+                </div>
+
+                <hr className="my-6 md:my-8 border-gray-200 " />
+
+                <p className="mb-6 text-gray-500 ">{details.description}</p>
+              </div>
+            </div>
+          </div>
+        </section>
+      )}
+    </div>
+  );
 };
 
 export default Details;
