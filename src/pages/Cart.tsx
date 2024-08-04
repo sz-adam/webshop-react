8b3e59b5dd65ee5c05d1b259bd45e56f2b5c073a
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-08-04 18:38:20 +0200

    cart style

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,7 +1,181 @@
 import React from "react";
+import { useCart } from "../context/CartContext";
+import { Link } from "react-router-dom";
+import { IoMdClose } from "react-icons/io";
+import { FaArrowRightLong } from "react-icons/fa6";
+import { BsPlusCircle } from "react-icons/bs";
+import { LuMinusCircle } from "react-icons/lu";
 
 const Cart: React.FC = () => {
-  return <h2>Cart</h2>;
+  const { cart } = useCart();
+
+  return (
+    <section className="py-8 antialiased  md:py-16">
+      <div className="mx-auto max-w-screen-xl px-4 2xl:px-0">
+        <h2 className="text-xl font-semibold text-gray-900  sm:text-2xl">
+          Shopping Cart
+        </h2>
+
+        <div className="mt-6 sm:mt-8 md:gap-6 lg:flex lg:items-start xl:gap-8">
+          <div className="mx-auto w-full flex-none lg:max-w-2xl xl:max-w-4xl">
+            {cart.map((product) => (
+              <div key={product.id} className="space-y-6">
+                <div className="rounded-lg border border-gray-200 bg-white p-4 shadow-sm md:p-6">
+                  <div className="space-y-4 md:flex md:items-center md:justify-between md:gap-6 md:space-y-0">
+                    <Link to="/" className="shrink-0 md:order-1">
+                      <img
+                        className="h-20 w-20 dark:hidden"
+                        src={product.image}
+                        alt={product.title}
+                      />
+                    </Link>
+
+                    <label className="sr-only">Choose quantity:</label>
+                    <div className="flex items-center justify-between md:order-3 md:justify-end">
+                      <div className="flex items-center">
+                        <button type="button">
+                          <LuMinusCircle className="h-5 w-5 text-gray-900 " />
+                        </button>
+                        <span className="w-10 shrink-0 border-0 bg-transparent text-center text-sm font-medium text-gray-900 focus:outline-none focus:ring-0 ">
+                          {" "}
+                          {product.quantity}
+                        </span>
+                        <button type="button">
+                          <BsPlusCircle className="h-5 w-5 text-gray-900 " />
+                        </button>
+                      </div>
+                      <div className="text-end md:order-4 md:w-32">
+                        <p className="text-base font-bold text-gray-900 ">
+                          {product.price}
+                        </p>
+                      </div>
+                    </div>
+
+                    <div className="w-full min-w-0 flex-1 space-y-4 md:order-2 md:max-w-md">
+                      <a
+                        href="#"
+                        className="text-base font-medium text-gray-900 hover:underline "
+                      >
+                        {product.title}
+                      </a>
+
+                      <div className="flex items-center gap-4">
+                        <button
+                          type="button"
+                          className="inline-flex items-center text-sm font-medium text-red-600 hover:underline"
+                        >
+                          <IoMdClose className="me-1.5 h-5 w-5" />
+                          Remove
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+
+          <div className="mx-auto mt-6 max-w-4xl flex-1 space-y-6 lg:mt-0 lg:w-full">
+            <div className="space-y-4 rounded-lg border border-gray-200 bg-white p-4 shadow-sm  sm:p-6">
+              <p className="text-xl font-semibold text-gray-900 ">
+                Order summary
+              </p>
+
+              <div className="space-y-4">
+                <div className="space-y-2">
+                  <dl className="flex items-center justify-between gap-4">
+                    <dt className="text-base font-normal text-gray-500 ">
+                      Original price
+                    </dt>
+                    <dd className="text-base font-medium text-gray-900 ">
+                      $7,592.00
+                    </dd>
+                  </dl>
+
+                  <dl className="flex items-center justify-between gap-4">
+                    <dt className="text-base font-normal text-gray-500 ">
+                      Savings
+                    </dt>
+                    <dd className="text-base font-medium text-green-600">
+                      -$299.00
+                    </dd>
+                  </dl>
+
+                  <dl className="flex items-center justify-between gap-4">
+                    <dt className="text-base font-normal text-gray-500 ">
+                      Store Pickup
+                    </dt>
+                    <dd className="text-base font-medium text-gray-900 ">
+                      $99
+                    </dd>
+                  </dl>
+
+                  <dl className="flex items-center justify-between gap-4">
+                    <dt className="text-base font-normal text-gray-500 ">
+                      Tax
+                    </dt>
+                    <dd className="text-base font-medium text-gray-900 ">
+                      $799
+                    </dd>
+                  </dl>
+                </div>
+
+                <dl className="flex items-center justify-between gap-4 border-t border-gray-200 pt-2 ">
+                  <dt className="text-base font-bold text-gray-900 ">Total</dt>
+                  <dd className="text-base font-bold text-gray-900 ">
+                    $8,191.00
+                  </dd>
+                </dl>
+              </div>
+
+              <a
+                href="#"
+                className="flex w-full items-center justify-center rounded-lg bg-primary-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-primary-800 focus:outline-none focus:ring-4 focus:ring-primary-300 "
+              >
+                Proceed to Checkout
+              </a>
+
+              <div className="flex items-center justify-center gap-2">
+                <span className="text-sm font-normal text-gray-500 "> or </span>
+                <a
+                  href="#"
+                  title=""
+                  className="inline-flex items-center gap-2 text-sm font-medium text-primary-700 underline hover:no-underline "
+                >
+                  Continue Shopping
+                  <FaArrowRightLong className="h-5 w-5" />
+                </a>
+              </div>
+            </div>
+
+            <div className="space-y-4 rounded-lg border border-gray-200 bg-white p-4 shadow-sm  sm:p-6">
+              <form className="space-y-4">
+                <div>
+                  <label className="mb-2 block text-sm font-medium text-gray-900 ">
+                    {" "}
+                    Do you have a voucher or gift card?{" "}
+                  </label>
+                  <input
+                    type="text"
+                    id="voucher"
+                    className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-primary-500 focus:ring-primary-500 "
+                    placeholder=""
+                    required
+                  />
+                </div>
+                <button
+                  type="submit"
+                  className="flex w-full items-center justify-center rounded-lg bg-primary-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-primary-800 focus:outline-none focus:ring-4 focus:ring-primary-300 "
+                >
+                  Apply Code
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Cart;
